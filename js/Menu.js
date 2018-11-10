89b69c229c41e2d9347b067b7e3e67127d32bee7
nilo3d <37418004+nilo3d@users.noreply.github.com>
2018-11-10 21:26:02 -0200

    adicionado variação da barra de menu
    Agora a barra do menu pode vir no topo ou embaixo, basta usar a classe apropriada.

--- js/Menu.js
+++ js/Menu.js
@@ -5,8 +5,24 @@ class Menu{
     constructor(menuId){
         this._menu = document.querySelector("#"+menuId);
     }
+
+    // Open/Close the global menu
     toggle(){
         this._menu.classList.toggle('menu-side-show');
+    }
+
+    // Ao clicar em uma opção do menu global, coloca o conteudo na section content, em seguida fecha o menu
+    click_item(item){
+        this._item = item.id;
+        menu.getContentPage(this._item);
+        menu.toggle();
+    }
+
+    // Pega o conteudo da página especificada
+    getContentPage(name){
+        $.get('../'+name, function(data){
+            $(".content").html(data);
+        });
     }
 }
 menu = new Menu('fly-global-menu');
